226bcb61749c8ebc4260ed7745ac19381f153cba
DijanU <pad23663@uvg.edu.gt>
2025-09-26 07:11:59 -0600

    porygon_2

---
+++ src/target/debug/deps/src-6a44e76cde07f8f3.d
@@ -0,0 +1,5 @@
+/home/dijan11/graficas/porygon/src/target/debug/deps/src-6a44e76cde07f8f3: src/main.rs
+
+/home/dijan11/graficas/porygon/src/target/debug/deps/src-6a44e76cde07f8f3.d: src/main.rs
+
+src/main.rs:
---
+++ src/target/debug/src.d
@@ -0,0 +1,1 @@
+/home/dijan11/graficas/porygon/src/target/debug/src: /home/dijan11/graficas/porygon/src/src/main.rs
